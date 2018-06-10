445dca6053844b6c955a6225fda545119b77512f
b0bbybaldi <roberto.baldizon@silibrain.com>
2018-06-10 13:00:26 -0700

    Added Jaws DB Link

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
 var connection;
 
+var JAWSDB_URL = "mysql://k2gcxf4sm31p782k:jez50gqrlui93t49@qzkp8ry756433yd4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/rwoteihwqefbndn8";
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
